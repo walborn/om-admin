7f389afbd80f65ba83977b9d876f96baeb24cb73
Борис Южаков <boris.yuzhakov@altarix.ru>
2019-04-10 16:03:24 +0300

    Updates

---
+++ precache-manifest.b2702123801102df688081b6751866e5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "491c1bd18904de2c4a3f",
+    "url": "/om-admin/static/css/main.777541b5.chunk.css"
+  },
+  {
+    "revision": "491c1bd18904de2c4a3f",
+    "url": "/om-admin/static/js/main.38a8c059.chunk.js"
+  },
+  {
+    "revision": "1198e791d8a279a2a976",
+    "url": "/om-admin/static/js/runtime~main.f5c6aa78.js"
+  },
+  {
+    "revision": "c58d27a47b41ffdafd7a",
+    "url": "/om-admin/static/css/2.f9a53a44.chunk.css"
+  },
+  {
+    "revision": "c58d27a47b41ffdafd7a",
+    "url": "/om-admin/static/js/2.1f24821f.chunk.js"
+  },
+  {
+    "revision": "b31fdba0fb1b9f2f2700eb87b0d7ef19",
+    "url": "/om-admin/static/media/ok.b31fdba0.svg"
+  },
+  {
+    "revision": "6b2f87a4312e11bb7af5f19c5948bedf",
+    "url": "/om-admin/static/media/loading.6b2f87a4.svg"
+  },
+  {
+    "revision": "791535bd93379471113874edc32249ba",
+    "url": "/om-admin/static/media/rip.791535bd.svg"
+  },
+  {
+    "revision": "63ff7ef8d075366c2cf754e465c4f23d",
+    "url": "/om-admin/static/media/trash.63ff7ef8.svg"
+  },
+  {
+    "revision": "31dcaf2f4b4a2f1f50e9c16e4761910b",
+    "url": "/om-admin/static/media/plus.31dcaf2f.svg"
+  },
+  {
+    "revision": "a9d0220ae7e61b7fe6d8f8c64d3cef0a",
+    "url": "/om-admin/static/media/duplicate.a9d0220a.svg"
+  },
+  {
+    "revision": "032b1af365202bbf27253f4bc7ca578f",
+    "url": "/om-admin/index.html"
+  }
+];
