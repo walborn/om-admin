913ac709b25816ca429cbd18ebf342b392a7acef
Борис Южаков <boris.yuzhakov@altarix.ru>
2019-04-12 21:39:07 +0300

    separate by dates

--- src/views/pages/event/template.js
+++ src/views/pages/event/template.js
@@ -8,6 +8,7 @@ import { ReactComponent as DuplicateSVG } from 'src/assets/svg/duplicate.svg';
 import { ReactComponent as DisableSVG } from 'src/assets/svg/disabled.svg';
 import { ReactComponent as UnDisableSVG } from 'src/assets/svg/undisabled.svg';
 import { ReactComponent as SubmitSVG } from 'src/assets/svg/submit.svg';
+import { ReactComponent as ArrowSVG } from 'src/assets/svg/arrow.svg';
 import 'react-datepicker/dist/react-datepicker.css';
 import './styles.scss';
 
@@ -27,10 +28,15 @@ const compare = (a, b) => {
 
 export default class Event extends React.Component {
     state = {
-        list: this.props.list.sort(compare),
+        list: [],
+        matrix: {},
+        opened: {},
         disabled: true,
         fakeId: 0,
     };
+    componentWillMount() {
+        this.setStateWithList({ list: this.props.list.sort(compare) })
+    }
     componentDidMount() {
         window.scrollTo(0, 0);
         this.props.fetchList();
@@ -38,15 +44,21 @@ export default class Event extends React.Component {
     componentWillReceiveProps(nextProps) {
         const { list } = nextProps;
         if (Array.isArray(list) && !isEqual(list, this.props.list)) {
-            this.setState({ list: list.sort(compare), disabled: true });
+            this.setStateWithList({ list: list.sort(compare), disabled: true })
         }
     }
+    setStateWithList = (newState) => {
+        if (!Array.isArray(newState.list)) return this.setState(newState);
+        const matrix = newState.list.reduce((res, i) => ({ ...res, [i.date]: [ ...(res[i.date] || []), i ] }), {});
+        this.setState({ matrix, ...newState });
+    };
+
     handleChange = id => name => ({ value }) => {
         const { list } = this.state;
         const i = list.findIndex(item => item.id === id);
         const nextList = [ ...list.slice(0, i), { ...list[i], [name]: value }, ...list.slice(i + 1) ];
         const disabled = isEqual(nextList, this.props.list);
-        this.setState({ list: nextList, disabled });
+        this.setStateWithList({ list: nextList, disabled });
     };
 
     handleCreate = () => {
@@ -56,7 +68,7 @@ export default class Event extends React.Component {
             id: fakeId + 1,
             disabled: 'false',
             type: '', time: '', duration: '', name: '', alternate: '', master: '', level: '', room: '', };
-        this.setState({ fakeId: fakeId + 1, list: [ ...list, emptyItem ], disabled: false });
+        this.setStateWithList({ fakeId: fakeId + 1, list: [ ...list, emptyItem ], disabled: false });
     };
 
     handleDelete = id => () => {
@@ -65,7 +77,7 @@ export default class Event extends React.Component {
         const nextList = [ ...list.slice(0, i), ...list.slice(i + 1) ];
         const disabled = isEqual(nextList, this.props.list);
 
-        this.setState({ list: nextList, disabled });
+        this.setStateWithList({ list: nextList, disabled });
     };
 
     handleDuplicate = item => () => {
@@ -74,58 +86,74 @@ export default class Event extends React.Component {
         const nextList = [ ...list.slice(0, i + 1), { ...item, id: fakeId + 1 }, ...list.slice(i + 1) ];
         const disabled = isEqual(nextList, this.props.list);
 
-        this.setState({ fakeId: fakeId + 1, list: nextList, disabled });
+        this.setStateWithList({ fakeId: fakeId + 1, list: nextList, disabled });
     };
 
     handleSubmit = () => {
         const prevList = this.props.list.reduce((res, i) => ({ ...res, [i.id]: i }), {});
         const nextList = this.state.list.reduce((res, i) => ({ ...res, [i.id]: i }), {});
-        this.setState({ disabled: true });
+        this.setStateWithList({ disabled: true });
         Promise.all(
             [
                 ...Object.keys(prevList).map(id => {
                     const nextItem = nextList[id];
                     if (!nextItem) return this.props.deleteItem(id);
-                    if (!isEqual(nextItem, prevList[id])) this.props.updateItem(nextItem);
-                }),
+                    if (!isEqual(nextItem, prevList[id])) return this.props.updateItem(nextItem);
+                }).filter(Boolean),
                 ...Object.keys(nextList).filter(id => !prevList[id]).map(id => this.props.createItem(nextList[id])),
             ]
         ).then(this.props.fetchList);
     };
 
+    handleOpenDate = date => () => this.setState({ opened: { ...this.state.opened, [date]: !this.state.opened[date] } });
+
     render() {
-        const { list, disabled } = this.state;
+        const { matrix, opened, disabled } = this.state;
+
         return (
             <div id="event-list">
                 {
-                    list.map((i) => (
-                        <div key={i.id} className={`item${i.disabled === 'true' ? ' disabled' : ''}`}>
-                            <div className="card">
-                                <DeleteSVG className="delete" onClick={this.handleDelete(i.id)} />
-                                <DuplicateSVG className="duplicate" onClick={this.handleDuplicate(i)} />
+                    Object.entries(matrix).map(([ date, classes ]) => (
+                        <div key={date} className={[ 'wrapper', opened[date] && 'opened'].filter(Boolean).join(' ')}>
+                            <div className="date" onClick={this.handleOpenDate(date)}>
+                                {date} ({classes.length})
+                                <ArrowSVG className="arrow" />
+                            </div>
+
+                            <div className="list">
                                 {
-                                    i.disabled === 'true'
-                                        ? <DisableSVG className="disable" onClick={() => this.handleChange(i.id)('disabled')({ value: 'false' })}/>
-                                        : <UnDisableSVG className="disable" onClick={() => this.handleChange(i.id)('disabled')({ value: 'true' })}/>
+                                    classes.map((i) => (
+                                        <div key={i.id} className={`item${i.disabled === 'true' ? ' disabled' : ''}`}>
+                                            <div className="card">
+                                                <DeleteSVG className="delete" onClick={this.handleDelete(i.id)} />
+                                                <DuplicateSVG className="duplicate" onClick={this.handleDuplicate(i)} />
+                                                {
+                                                    i.disabled === 'true'
+                                                        ? <DisableSVG className="disable" onClick={() => this.handleChange(i.id)('disabled')({ value: 'false' })}/>
+                                                        : <UnDisableSVG className="disable" onClick={() => this.handleChange(i.id)('disabled')({ value: 'true' })}/>
+                                                }
+                                                <div className="card__field card__field--date">
+                                                    <DatePicker
+                                                        selected={toDate(i.date)}
+                                                        onChange={(date) => {
+                                                            if (!date) return;
+                                                            const value = date.toLocaleDateString('ru');
+                                                            this.handleChange(i.id)('date')({ value });
+                                                        }}
+                                                        dateFormat="dd.MM.yy"
+                                                    />
+                                                </div>
+                                                <TimeInput className="card__field card__field--time" placeholder="Time" title="time" value={i.time} onChange={this.handleChange(i.id)('time')}/>
+                                                <Input className="card__field" placeholder="Type" title="type" value={i.type} onChange={this.handleChange(i.id)('type')}/>
+                                                <Input className="card__field" placeholder="Duration" title="duration" value={i.duration} onChange={this.handleChange(i.id)('duration')}/>
+                                                <Input className="card__field" placeholder="Title" title="title" value={i.name} onChange={this.handleChange(i.id)('name')}/>
+                                                <Input className="card__field" placeholder="Alternate" title="alternate" value={i.alternate} onChange={this.handleChange(i.id)('alternate')}/>
+                                                <Input className="card__field" placeholder="Master" title="master" value={i.master} onChange={this.handleChange(i.id)('master')}/>
+                                                <Input className="card__field" placeholder="Level" title="level" value={i.level} onChange={this.handleChange(i.id)('level')}/>
+                                            </div>
+                                        </div>
+                                    ))
                                 }
-                                <div className="card__field card__field--date">
-                                    <DatePicker
-                                        selected={toDate(i.date)}
-                                        onChange={(date) => {
-                                            if (!date) return;
-                                            const value = date.toLocaleDateString('ru');
-                                            this.handleChange(i.id)('date')({ value });
-                                        }}
-                                        dateFormat="dd.MM.yy"
-                                    />
-                                </div>
-                                <TimeInput className="card__field card__field--time" placeholder="Time" title="time" value={i.time} onChange={this.handleChange(i.id)('time')}/>
-                                <Input className="card__field" placeholder="Type" title="type" value={i.type} onChange={this.handleChange(i.id)('type')}/>
-                                <Input className="card__field" placeholder="Duration" title="duration" value={i.duration} onChange={this.handleChange(i.id)('duration')}/>
-                                <Input className="card__field" placeholder="Title" title="title" value={i.name} onChange={this.handleChange(i.id)('name')}/>
-                                <Input className="card__field" placeholder="Alternate" title="alternate" value={i.alternate} onChange={this.handleChange(i.id)('alternate')}/>
-                                <Input className="card__field" placeholder="Master" title="master" value={i.master} onChange={this.handleChange(i.id)('master')}/>
-                                <Input className="card__field" placeholder="Level" title="level" value={i.level} onChange={this.handleChange(i.id)('level')}/>
                             </div>
                         </div>
                     ))
